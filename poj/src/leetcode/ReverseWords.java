c3babdf506cd8047457ab5335c6024e402802fc0
Ben Wang <ben.wang01@sap.com>
2014-09-03 17:51:20 +0800

    ac  file

--- poj/src/leetcode/ReverseWords.java
+++ poj/src/leetcode/ReverseWords.java
@@ -4,17 +4,24 @@ public class ReverseWords {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        String source="hello,world";
+        String source="hello  ssss   ";
         String dst=reverseWords(source);
         System.out.println(dst);
     }
 
     public static String reverseWords(String s) {
-        int i, len = s.length();
-        StringBuffer dest = new StringBuffer(len);
-        for (i = (len - 1); i >= 0; i--)
-            dest.append(s.charAt(i));
-        return dest.toString();
+       if(s.trim().equals("")||s==null) return "";
+        
+        String[] words = s.split(" "); //Get every single word.
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!"".equals(words[i])) {
+                sb.append(words[i] + " ");
+            }
+        }
+        
+        return sb.length() == 0 ? "" : sb.toString().substring(0, sb.length() - 1);
     }
 
 }
