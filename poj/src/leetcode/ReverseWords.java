c19e7e397ac2aa89b8262aa933b9f4c934fcd0cd
Ben Wang <ben.wang01@sap.com>
2014-09-03 17:30:37 +0800

    add  reverseWords

--- poj/src/leetcode/ReverseWords.java
+++ poj/src/leetcode/ReverseWords.java
@@ -4,7 +4,17 @@ public class ReverseWords {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        String source="hello,world";
+        String dst=reverseWords(source);
+        System.out.println(dst);
+    }
 
+    public static String reverseWords(String s) {
+        int i, len = s.length();
+        StringBuffer dest = new StringBuffer(len);
+        for (i = (len - 1); i >= 0; i--)
+            dest.append(s.charAt(i));
+        return dest.toString();
     }
 
 }
